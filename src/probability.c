2293b31cde4f31eb3ade78a259159945f0008fbb
Simon Petit <nomisp96@hotmail.fr>
2018-07-09 14:24:47 +0200

    MAJOR beginning the probability side of the library with mean, variance and covariance for vectors so far

---
+++ src/probability.c
@@ -0,0 +1,36 @@
+#include "probability.h"
+
+double v_mean(vector *v)
+{
+    double mean = 0;
+    int i;
+    for (i = 0; i < v_size(v); ++i) {
+        mean += v_get(v, i);
+    }
+    mean = mean/v_size(v);
+    return mean;
+}
+
+double v_variance(vector *v)
+{
+    return v_covariance(v, v);
+}
+
+double v_covariance(vector *u, vector *v)
+{
+    double mean1, mean2, covar = 0;
+    int i;
+    mean1 = v_mean(u);
+    mean2 = v_mean(v);
+
+    //TODO: handle error in case both vector are not of the same dimension
+
+    for (i = 0; i < v_size(u); ++i) {
+        covar += (v_get(u, i) - mean1)*(v_get(v, i) - mean2);
+    }
+
+    covar = covar / v_size(u);
+
+    return covar;
+}
+
---
+++ src/probability.h
@@ -0,0 +1,12 @@
+#ifndef PROBABILITY_H
+#define PROBABILITY_H
+
+#include "vector.h"
+
+double v_mean(vector *v);
+
+double v_variance(vector *v);
+
+double v_covariance(vector *u, vector *v);
+
+#endif
